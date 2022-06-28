3b82410d0614ddbb3f880bb4ee404f8c03e05fb9
Bassa1 <100380537+Bassa1@users.noreply.github.com>
2022-06-28 10:08:12 -0700

    Add files via upload
    The updated parser-based on Yao's feedback, clear the main and mlt, assigned the data folder and

---
+++ Ehr_kp_parser_v3.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+# Import Packages
+import numpy as np
+import pandas as pd
+import sys, os
+import json
+import csv
+
+
+# In[14]:
+
+
+def load_tsv_data(data_folder):
+    """
+    This function takes two parameters: edge_filepath and the node_filepath.
+    It will return a json formats that has the key of id, subject, predicate, object as the main keys. 
+    """
+    node_filepath = os.path.join(data_folder, "clinical_risk_ehr_risk_nodes_data_2022_06_01.csv")
+    edge_filepath = os.path.join(data_folder, "clinical_risk_ehr_risk_edges_data_2022_06_01.csv")
+    edges_data = pd.read_csv(edge_filepath, sep = ',')
+    nodes_data = pd.read_csv(node_filepath, sep = ',')
+    cut_off = None
+    # generate the nodes atributes
+    node_name_mapping = {}
+    node_type_mapping = {}
+    node_xref_mapping = {}
+    for index, row in nodes_data.iterrows():
+        node_name_mapping[row["id"]] = row["name"]
+        node_type_mapping[row["id"]] = row["category"].split(':')[1] if str(row["category"]).startswith("biolink:") else row["category"]
+        node_xref_mapping[row["id"]] = row["xref"]
+        
+    # generate the edges attributes
+    for index, row in edges_data.iterrows():
+        if row["subject"] and row["predicate"] and row["subject"].split(':')[0] and row["object"].split(':')[0]:
+            # Specify properties for subject
+            subject = {
+                row["subject"].split(':')[0]: row["subject"],
+                "id": row["subject"],
+                "name": node_name_mapping[row["subject"]],
+                "type": node_type_mapping[row["subject"]]
+            }
+#             current_xref = node_xref_mapping[row["subject"]]
+            if ~np.isnan(node_xref_mapping[row["subject"]]):
+                subject[node_xref_mapping[row["subject"]].split(':')[0]] =  node_xref_mapping[row["subject"]]
+
+            # Specify properties for object
+            objects = {
+                row["object"].split(':')[0]: row["object"],
+                "id": row["object"],
+                "name": node_name_mapping[row["object"]],
+                "type": node_type_mapping[row["object"]]
+            }
+            if ~np.isnan(node_xref_mapping[row["object"]]):
+                objects[node_xref_mapping[row["object"]].split(':')[0]] =  node_xref_mapping[row["object"]]
+
+            # Specify properties for predicate
+
+            association = {
+                "predicate": row["predicate"].split(':')[-1] if row["predicate"].startswith("biolink:") else row["predicate"],
+#                     "original": row["predicate"],
+                "provided_by": "EHR Risk Provider (Multiomics)",
+                "provided_date": row["provided_date"],
+                "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/EHR-Risk-KP",
+#                     "category": row["category"].split(':')[1] if row["category"].startswith("biolink:") else row["category"],
+                "classifier": row["classifier"],
+                "original_predicate": row["relation"],
+                "auc_roc": float(row["auc_roc"]),
+                "p_values": float(row["p_value"])
+            }
+
+            # conditional properties for predicate (if not Null)
+            if ~np.isnan(row["feature_importance"]):
+                association["feature_importance"] =  float(row["feature_importance"])
+            if ~np.isnan(row["feature_coefficient"]):
+                association["feature_coefficient"] = float(row["feature_coefficient"])
+                association["odd_ratio"] = np.exp(row["feature_coefficient"])
+
+            # make a unique id from  subject, predicate, and object indentifiers
+#                 if row["classifier"] == "Logistic Regression":
+            unique_id_list = [row["subject"], row["predicate"], row["object"]]
+#                 else:
+#                     unique_id_list = [row["subject"], row["predicate"], row["object"]]
+        json = {
+            "id":'-'.join(unique_id_list),
+            "subject": subject,
+            "association": association,
+            "object": objects
+        }
+        yield json
+
+
+# In[ ]:
+
+
+
+
