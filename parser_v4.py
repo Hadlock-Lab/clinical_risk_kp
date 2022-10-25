4e8a9abdbdcc9f421ef5d78df2616684cb6ddf13
Bassa1 <100380537+Bassa1@users.noreply.github.com>
2022-10-25 14:46:17 -0700

    Update parser_v4.py

--- parser_v4.py
+++ parser_v4.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!#!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[16]:
 
 
 #!/usr/bin/env python
@@ -32,65 +32,64 @@ def load_tsv_data(data_folder):
         node_type_mapping[row["id"]] = row["category"].split(':')[1] if str(row["category"]).startswith("biolink:") else row["category"]
         node_xref_mapping[row["id"]] = row["xref"] 
     # generate the edges attributes
-    for index, row in edges_data.iterrows():
-        if row['pvalue']<=0.05:
-            if row["subject"] and row["predicate"] and str(row["subject"]).split(':')[0] and str(row["object"]).split(':')[0]:
-                # Specify properties for subject
-                subject = {
-                    str(row["subject"]).split(':')[0]: row["subject"],
-                    "id": row["subject"],
-                    "name": node_name_mapping[row["subject"]],
-                    "type": node_type_mapping[row["subject"]] if pd.notnull(node_type_mapping[row["subject"]]) else "Unknown/Missing"
-                }
-    #             current_xref = node_xref_mapping[row["subject"]]
-                if ~np.isnan(node_xref_mapping[row["subject"]]):
-                    subject[node_xref_mapping[row["subject"]].split(':')[0]] =  node_xref_mapping[row["subject"]]
-                # Specify properties for object
-                objects = {
-                    str(row["object"]).split(':')[0]: row["object"],
-                    "id": row["object"],
-                    "name": node_name_mapping[row["object"]],
-                    "type": node_type_mapping[row["object"]] if pd.notnull(node_type_mapping[row["object"]]) else "Unknown/Missing"
-                }
-                object_aspect = {row["object_aspect"].split(':')[0]: row["object_aspect"].split(':')[-1] if row["object_aspect"].startswith("biolink:") else row["object_aspect"]}
-                object_direction = {row["object_direction"].split(':')[0]:row["object_direction"].split(':')[-1] if row["object_direction"].startswith("biolink:") else row["object_direction"]}
-                if ~np.isnan(node_xref_mapping[row["object"]]):
-                    objects[node_xref_mapping[row["object"]].split(':')[0]] =  node_xref_mapping[row["object"]]
-                # Specify properties for predicate
-                association = {
-                    "predicate": row["predicate"].split(':')[-1] if row["predicate"].startswith("biolink:") else row["predicate"],
-    #                     "original": row["predicate"],
-                    "provided_date": row["provided_date"],
-                    "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/EHR-Risk-KP",
-    #                     "category": row["category"].split(':')[1] if row["category"].startswith("biolink:") else row["category"],
-                    "classifier": row["classifier"],
-                    "original_predicate": row["relation"],
-                    "auc_roc": float(row["auc_roc"]),
-                    "p_values": float(row["pvalue"])
-                }
-                # conditional properties for predicate (if not Null)
-                if ~np.isnan(row["feature_importance"]):
-                    association["feature_importance"] =  float(row["feature_importance"])
-                if ~np.isnan(row["feature_coefficient"]):
-                    association["feature_coefficient"] = float(row["feature_coefficient"])
-                    association["odd_ratio"] = np.exp(row["feature_coefficient"])
+    for index, row in edges_data[edges_data['pvalue']<=0.05].iterrows():
+#         if row['pvalue']<=0.05:
+        if row["subject"] and row["predicate"] and str(row["subject"]).split(':')[0] and str(row["object"]).split(':')[0]:
+            # Specify properties for subject
+            subject = {
+                row["subject"].split(':')[0]: row["subject"],
+                "id": row["subject"],
+                "name": node_name_mapping[row["subject"]],
+                "type": node_type_mapping[row["subject"]] if pd.notnull(node_type_mapping[row["subject"]]) else "Unknown/Missing"
+            }
+#             current_xref = node_xref_mapping[row["subject"]]
+            if ~np.isnan(node_xref_mapping[row["subject"]]):
+                subject[node_xref_mapping[row["subject"]].split(':')[0]] =  node_xref_mapping[row["subject"]]
+            # Specify properties for object
+            objects = {
+                row["object"].split(':')[0]: row["object"],
+                "id": row["object"],
+                "name": node_name_mapping[row["object"]],
+                "type": node_type_mapping[row["object"]] if pd.notnull(node_type_mapping[row["object"]]) else "Unknown/Missing"
+            }
+            object_aspect = {row["object_aspect"].split(':')[0]: row["object_aspect"].split(':')[-1] if row["object_aspect"].startswith("biolink:") else row["object_aspect"]}
+            object_direction = {row["object_direction"].split(':')[0]:row["object_direction"].split(':')[-1] if row["object_direction"].startswith("biolink:") else row["object_direction"]}
+            if ~np.isnan(node_xref_mapping[row["object"]]):
+                objects[node_xref_mapping[row["object"]].split(':')[0]] =  node_xref_mapping[row["object"]]
+            # Specify properties for predicate
+            association = {
+                "predicate": row["predicate"].split(':')[-1] if row["predicate"].startswith("biolink:") else row["predicate"],
+#                     "original": row["predicate"],
+                "provided_date": row["provided_date"],
+                "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/EHR-Risk-KP",
+#                     "category": row["category"].split(':')[1] if row["category"].startswith("biolink:") else row["category"],
+                "classifier": row["classifier"],
+                "original_predicate": row["relation"],
+                "auc_roc": float(row["auc_roc"]),
+                "p_values": float(row["pvalue"])
+            }
+            # conditional properties for predicate (if not Null)
+            if ~np.isnan(row["feature_importance"]):
+                association["feature_importance"] =  float(row["feature_importance"])
+            if ~np.isnan(row["feature_coefficient"]):
+                association["feature_coefficient"] = float(row["feature_coefficient"])
+                association["odd_ratio"] = np.exp(row["feature_coefficient"])
 
-                # make a unique id from  subject, predicate, and object indentifiers
-                if row["classifier"] == "Logistic Regression":
-                    unique_id_list = [row["subject"], row["predicate"],row["object_aspect"],row["object_direction"], row["object"], row["classifier"], str(row["auc_roc"]), str(row["feature_coefficient"])]
-                else:
-                    unique_id_list = [row["subject"], row["object_direction"],row["object_aspect"],row["object_direction"], row["object"], row["classifier"], str(row["auc_roc"]),str(row["feature_importance"])]
-                json = {
-                    "_id":'-'.join(unique_id_list),
-                    "subject": subject,
-                    "association": association,
-                    "object": objects,
-                    "object_aspect": object_aspect,
-                    "object_direction":object_direction
-                    }
-                yield json
-
-
+            # make a unique id from  subject, predicate, and object indentifiers
+            if row["classifier"] == "Logistic Regression":
+                unique_id_list = [row["subject"], row["predicate"],row["object_aspect"],row["object_direction"], row["object"], row["classifier"], str(row["auc_roc"]), str(row["feature_coefficient"])]
+            else:
+                unique_id_list = [row["subject"], row["object_direction"],row["object_aspect"],row["object_direction"], row["object"], row["classifier"], str(row["auc_roc"]),str(row["feature_importance"])]
+            json = {
+                "_id":'-'.join(unique_id_list),
+                "subject": subject,
+                "association": association,
+                "object": objects,
+                "object_aspect": object_aspect,
+                "object_direction":object_direction
+                }
+            yield json
+            
 # In[9]:
 
 
